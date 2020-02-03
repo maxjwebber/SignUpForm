5c34289c3a868f513170f83088bbb3803b719455
maxjwebber <tua83483@temple.edu>
2020-02-03 16:03:27 -0500

    Added new EditText views, labels and button

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,6 +3,8 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void confirmMessage(View v)
+    {
+        Toast confirmMessage = Toast.makeText(getApplicationContext(),"CONFIRMED FOR BRAWL",Toast.LENGTH_SHORT);
+        confirmMessage.show();
     }
 }
